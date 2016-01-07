f882b81bb3c1c7f3d1822983d660e8fd9178dcdb
Nivedha7 <nivy77@gmail.com>
2016-01-06 19:26:30 -0800

    Create Cylinder.java

---
+++ Cylinder.java
@@ -0,0 +1,26 @@
+public class Cylinder extends Cone{
+	
+	private double height;
+	private double radius;
+
+	public Cone(double radius, double height){
+		super(radius, height);
+		this.radius = radius;
+		this.height = height;
+	}
+
+	@Override
+	public double getVolume(){
+		return Math.PI * radius * radius * height;
+	}
+
+	@Override
+	public double getSurfaceArea(){
+		return (2 * Math.PI * radius * height) + (2 * Math.PI * radius); 
+	}
+
+	public double getLateralArea(){
+		return 2 * Math.PI * radius * height;
+	}
+
+}
