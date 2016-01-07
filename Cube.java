a18672b7743658b68bfdbddee4d02d369c15d298
ananda1066 <alishananda@gmail.com>
2016-01-06 18:25:35 -0800

    extended RectangularPrism

--- Cube.java
+++ Cube.java
@@ -1,19 +1,23 @@
-public class Cube implements Shapes{
+public class Cube extends RectangularPrism{
 	
 	private double side;
 
 	public Cube(double width){
+		super(width);
 		this.side = width;
 	}
 
+	@Override
 	public double getVolume() {
 		return Math.pow(side, 3);
 	}
 
+	@Override
 	public double getSurfaceArea() {
 		return 6*(side * side);
 	}
 
+	@Override
 	public double getPerimeter() {
 		return 12*side;
 	}
